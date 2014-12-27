e5b9680ece910760873039be970610343a7196a7
Ellie Hoshizaki <ellie@mediahound.com>
2014-12-27 00:08:46 -0800

    added assets - still need to optimize

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,86 @@
+// ------------------------------------------
+// jQuery - on page load
+// ------------------------------------------
+$(document).ready(function() {
 
+  // Affix nav
+  $(window).scroll(function(){
+    var scrollTop = $('.home').height();
+    if($(window).scrollTop() >= scrollTop){
+      $('#nav-container').css({
+        visibility: 'visible',
+        position: 'fixed',
+        top: '0'
+      });
+    }
+    if($(window).scrollTop() < scrollTop){
+      $('#nav-container').removeAttr('style');  
+    }
+  })
+
+  // For Parallax Background with Cursor Movement
+  $('#blackBackdrop').mousemove(function(e){
+    var amountMovedX = (e.pageX * -1 / 6);
+    var amountMovedY = (e.pageY * -1 / 6);
+    $('#landing-content').css('left', amountMovedX * 4);
+    $('#landing-content').css('top', amountMovedY);
+    // $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+  });
+
+  // For Parallax Box1 with Cursor Movement
+  $('#blackBackdrop').mousemove(function(e){
+    var amountMovedX = (e.pageX * 1 / 4);
+    var amountMovedY = (e.pageY * 1 / 4);
+    $('#movement2').css('left', amountMovedX * 2);
+    $('#movement2').css('top', amountMovedY * 2);
+    $('#movement3').css('left', amountMovedX);
+    $('#movement3').css('top', amountMovedY);
+  });
+
+  // Sticky nav
+  $('body').addClass('js');
+  var $nav = $('#nav'),
+    $menulink = $('.menu-link');
+  
+  $menulink.click(function() {
+    console.log("clicked");
+    $menulink.toggleClass('active');
+    $nav.toggleClass('active');
+    return false;
+  });
+
+  // jQuery Scroll Animation
+  $(window).scroll(function(){
+    $('.servicesIcon').each(function(){
+      var iconPos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+        if (iconPos < topOfWindow+600) {
+          $(this).addClass("bounceIn");
+        }
+    });
+  });
+
+  //   $(window).bind('scroll',function(e){
+  //     parallaxScroll();
+  // });
+ 
+  // function parallaxScroll(){
+  //   var scrolled = $(window).scrollTop();
+
+  //   $('#works').css('top',(4-(scrolled*.9))+'px');
+  //   $('#portfolioContainer').css('top',(9-(scrolled*.9))+'px');
+  // }
+
+  // Animate Nav Scroll
+  $("#nav ul li a[href^='#']").on('click', function(e){
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 300, function() {
+      window.location.hash = hash;
+    });
+  });
+
+});
